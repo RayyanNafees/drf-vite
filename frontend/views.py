2080873caeb69aa6f2e95994566267aedd284698
RayyanNafees <nafees.rayyan@gmail.com>
2023-07-02 21:34:28 +0530

    Added Project Name to Website title

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'frontend/index.html')
+    return render(
+        request, "frontend/index.html", context={"project_name": "{{ project_name }}"}
+    )
