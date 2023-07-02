8f2cab6dc3b5d132670ad845a57e0c72ce4a35ee
RayyanNafees <nafees.rayyan@gmail.com>
2023-07-02 21:36:34 +0530

    Added title cased project name as Index.html title

--- frontend/views.py
+++ frontend/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(
-        request, "frontend/index.html", context={"project_name": "{{ project_name }}"}
+        request,
+        "frontend/index.html",
+        context={"project_name": "{{ project_name }}".replace("-", " ").title()},
     )
